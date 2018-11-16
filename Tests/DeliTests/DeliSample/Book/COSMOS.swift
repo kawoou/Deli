fa71a796ec4693168f72da6cc4dd7052549fa9f6
kawoou <kawoou@kawoou.kr>
2018-11-16 11:29:22 +0900

    Fix build failed.

--- Tests/DeliTests/DeliSample/Book/COSMOS.swift
+++ Tests/DeliTests/DeliSample/Book/COSMOS.swift
@@ -1,6 +1,6 @@
 import Deli
 
 struct COSMOS: Science, Component {
-    var qualifier: String? { return nil }
+    var qualifier: String? { return "Science" }
     var scope: Scope { return .singleton }
 }
