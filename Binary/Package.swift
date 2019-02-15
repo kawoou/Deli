b17c66f3b8da7e1315929c785c95d0033e84ba76
kawoou <kawoou@kawoou.kr>
2019-02-15 17:06:17 +0900

    Update libraries version.

--- Binary/Package.swift
+++ Binary/Package.swift
@@ -7,10 +7,10 @@ let package = Package(
         .executable(name: "deli", targets: ["deli"])
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.1"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.22.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.1"),
         .package(url: "https://github.com/crossroadlabs/Regex.git", from: "1.1.0"),
-        .package(url: "https://github.com/tuist/xcodeproj.git", from: "6.0.0"),
+        .package(url: "https://github.com/tuist/xcodeproj.git", from: "6.5.0"),
         .package(url: "https://github.com/Carthage/Commandant.git", from: "0.15.0")
     ],
     targets: [
